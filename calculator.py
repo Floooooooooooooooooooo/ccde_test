b2c699d9db45c64c5bcd5c99b87f36de30180c82
Floooooooooooooooooooo <fkainz@tsn.at>
2026-06-01 10:11:07 +0200

    added mul & div function

--- calculator.py
+++ calculator.py
@@ -9,3 +9,15 @@ def sub(a, b):
 
 if __name__ == "__main__":
     print("5 - 3 =", sub(5, 3))
+
+def mul(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("5 * 3 =", mul(5, 3))
+
+def div(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print("5 / 3 =", div(5, 3))
